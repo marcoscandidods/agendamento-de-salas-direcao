40d771f052c1107c6a6963af03be525440fa1015
marcoscandidods <marcoscandidods@gmail.com>
2026-04-11 00:14:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ def login_dialog():
 
 col_t, col_l = st.columns([7, 3])
 with col_t:
-    st.title("🏛️ Gestão de Espaços - FES")
+    st.title("📅 Gestão de Espaços - FES")
 with col_l:
     if st.session_state.user:
         st.write(f"Olá, **{st.session_state.user}**")
